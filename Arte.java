44e197467dc81d7bf96f0d6277a7b43fe42d580b
juliaaamendes <juliaaa.mendes.p@gmail.com>
2025-05-19 10:14:22 -0300

    novaArte

--- Arte.java
+++ Arte.java
@@ -1,13 +1,5 @@
 public abstract class Arte {
-    private String nome;
-    
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
     public abstract String getPintura();
     public abstract String getEscultura();
 }
+
---
+++ ArteRomana.java
@@ -0,0 +1,11 @@
+public class ArteRomana extends Arte {
+    @Override
+    public String getPintura() {
+        return "Pintura Romana";
+    }
+
+    @Override
+    public String getEscultura() {
+        return "Escultura Romana";
+    }
+}
